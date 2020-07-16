42061413c5808ee95421df3779e204876c6221ca
awesomespidey <45442042+awesomespidey@users.noreply.github.com>
2020-07-15 19:30:59 -0700

    defrgthyjy

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,14 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+public static void main(String[] args) {
+	JFrame frame;
+	public static final ( int HEIGHT); 
+	public static final ( int WIDTH); 
+	
+	
+	
+	
+}
 
 }
