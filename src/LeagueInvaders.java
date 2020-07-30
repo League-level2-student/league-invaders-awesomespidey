8f2bd849c6729c4ce5ce5316c7210a317fc79767
awesomespidey <45442042+awesomespidey@users.noreply.github.com>
2020-07-29 19:28:06 -0700

    ferkmdkm

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,13 +1,19 @@
 import javax.swing.JFrame;
 
 public class LeagueInvaders {
-public static void main(String[] args) {
+	
+	public static final int HEIGHT = 800; 
+	public static final int WIDTH = 500;
 	JFrame frame;
-	public static final ( int HEIGHT); 
-	public static final ( int WIDTH); 
-	
-	
-	
+	LeagueInvaders(){
+		frame = new JFrame();
+	}
+	void setup() {
+		HEIGHT;
+		WIDTH;
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	public static void main(String[] args) {
 	
 }
 
