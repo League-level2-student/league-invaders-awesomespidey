8fcd5dd22ab12180d915fc808b960b7dd0704652
awesomespidey <45442042+awesomespidey@users.noreply.github.com>
2020-08-26 19:26:22 -0700

    se5r6ut7jghnfbdvrfey6tukhgnfbe

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -5,8 +5,27 @@ import javax.swing.JPanel;
 public class GamePanel extends JPanel {
 	@Override
 	public void paintComponent(Graphics g){
-
+		g.fillRect(10, 10, 100, 100);
 	}
-	g.fillRect(10, 10, 100, 100);
-	
+	   final int MENU = 0;
+	   final int GAME = 1;
+	   final int END = 2;
+	   
+	   int currentState = MENU;
+	   
+	   void updateMenuState() {  }
+	   void updateGameState() {  }
+	   void updateEndState()  {  }
+	   
+	   void drawMenuState(Graphics g) {  }
+	   void drawGameState(Graphics g) {  }
+	   void drawEndState(Graphics g)  {  }
+	   
+	   if(currentState == MENU){
+		    drawMenuState(g);
+		}else if(currentState == GAME){
+		    drawGameState(g);
+		}else if(currentState == END){
+		    drawEndState(g);
+		}
 }
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -5,18 +5,19 @@ public class LeagueInvaders {
 	public static final int HEIGHT = 800; 
 	public static final int WIDTH = 500;
 	JFrame frame;
+	GamePanel gpanel;
 	LeagueInvaders(){
 		frame = new JFrame();
+		gpanel = new GamePanel();
 	}
-	void setup() {
-		HEIGHT;
-		WIDTH;
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	void setup () {
+		frame.add(gpanel);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
 	public static void main(String[] args) {
-		LeagueInvaders();
-		setup();
+		new LeagueInvaders().setup();
 }
 
-}                                                                      
-                     
+}
