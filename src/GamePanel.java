2971d7fd40420a545d3bbbcb21fc905e82c81669
awesomespidey <45442042+awesomespidey@users.noreply.github.com>
2020-09-02 19:26:11 -0700

    jhfghghgjuyhhrgg

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,31 +1,48 @@
+import java.awt.Color;
 import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
-	@Override
-	public void paintComponent(Graphics g){
-		g.fillRect(10, 10, 100, 100);
+	final int MENU = 0;
+	final int GAME = 1;
+	final int END = 2;
+
+	int currentState = MENU;
+
+	void updateMenuState() {
 	}
-	   final int MENU = 0;
-	   final int GAME = 1;
-	   final int END = 2;
-	   
-	   int currentState = MENU;
-	   
-	   void updateMenuState() {  }
-	   void updateGameState() {  }
-	   void updateEndState()  {  }
-	   
-	   void drawMenuState(Graphics g) {  }
-	   void drawGameState(Graphics g) {  }
-	   void drawEndState(Graphics g)  {  }
-	   
-	   if(currentState == MENU){
-		    drawMenuState(g);
-		}else if(currentState == GAME){
-		    drawGameState(g);
-		}else if(currentState == END){
-		    drawEndState(g);
+
+	void updateGameState() {
+	}
+
+	void updateEndState() {
+	}
+
+	void drawMenuState(Graphics g) {
+		g.setColor(Color.BLUE);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+	}
+
+	void drawGameState(Graphics g) {
+		g.setColor(Color.black);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+	}
+
+	void drawEndState(Graphics g) {
+		g.setColor(Color.red);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+	}
+
+	public void paintComponent(Graphics g) {
+		g.fillRect(10, 10, 100, 100);
+
+		if (currentState == MENU) {
+			drawMenuState(g);
+		} else if (currentState == GAME) {
+			drawGameState(g);
+		} else if (currentState == END) {
+			drawEndState(g);
 		}
+	}
 }
